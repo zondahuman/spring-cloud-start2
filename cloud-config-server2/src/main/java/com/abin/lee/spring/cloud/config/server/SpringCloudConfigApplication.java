15622bb4e110906a8d912c7f579bbaf4a78d09a4
zondahuman <zondahuman@gmail.com>
2018-01-17 22:56:28 +0800

    add

--- cloud-config-server2/src/main/java/com/abin/lee/spring/cloud/config/server/SpringCloudConfigApplication.java
+++ cloud-config-server2/src/main/java/com/abin/lee/spring/cloud/config/server/SpringCloudConfigApplication.java
@@ -1,7 +1,7 @@
 package com.abin.lee.spring.cloud.config.server;
 
 /**
- * Created by abin on 2018/1/17 2018/1/17.
+ * Created by abin on 2018/1/17 22:56.
  * spring-cloud-start2
  * com.abin.lee.spring.cloud.config.server
  */
--- cloud-config-server2/src/test/java/com/abin/lee/spring/cloud/config/server/test/AppTest.java
+++ cloud-config-server2/src/test/java/com/abin/lee/spring/cloud/config/server/test/AppTest.java
@@ -7,5 +7,5 @@ package com.abin.lee.spring.cloud.config.server.test;
  */
 public class AppTest {
 
-    
+
 }
