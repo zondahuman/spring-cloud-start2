d7a7a9fc81c3c28d0db60e4f05c77de1c29f6c05
zondahuman <zondahuman@gmail.com>
2018-07-13 00:27:56 +0800

    add

---
+++ cloud-monitor-admin2/src/main/java/com/abin/lee/cloud/monitor/admin/CloudMonitorAdminApplication.java
@@ -0,0 +1,21 @@
+package com.abin.lee.cloud.monitor.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by abin on 2018/7/12.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class CloudMonitorAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudMonitorAdminApplication.class, args);
+    }
+
+
+}
