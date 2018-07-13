4cf2d1442792a2a09c8d7ad172956ab7e0bc1465
zondahuman <zondahuman@gmail.com>
2018-07-13 11:50:41 +0800

    add

--- cloud-dashboard-zipkin2/src/main/java/com/abin/lee/cloud/zipkin/dashboard/ZipkinDashboardCloudApplication.java
+++ cloud-dashboard-zipkin2/src/main/java/com/abin/lee/cloud/zipkin/dashboard/ZipkinDashboardCloudApplication.java
@@ -1,10 +1,13 @@
 package com.abin.lee.cloud.zipkin.dashboard;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import zipkin.server.EnableZipkinServer;
+import zipkin.storage.mysql.MySQLStorage;
+
+import javax.sql.DataSource;
 
 /**
  * Created by abin on 2018/7/12.
@@ -22,6 +25,11 @@ public class ZipkinDashboardCloudApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(ZipkinDashboardCloudApplication.class).web(true).run(args);
+    }
+
+    @Bean
+    public MySQLStorage mySQLStorage(DataSource datasource) {
+        return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
     }
 
     /**
