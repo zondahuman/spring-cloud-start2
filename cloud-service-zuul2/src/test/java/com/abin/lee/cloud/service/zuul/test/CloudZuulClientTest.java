225b0d88ca51bdbd826159f28a8f2fe55df42355
zondahuman <zondahuman@gmail.com>
2018-08-14 20:34:23 +0800

    add

--- cloud-service-zuul2/src/test/java/com/abin/lee/cloud/service/zuul/test/CloudZuulClientTest.java
+++ cloud-service-zuul2/src/test/java/com/abin/lee/cloud/service/zuul/test/CloudZuulClientTest.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 /**
  * Created by abin on 2018/7/12.
- *  http://localhost:9477/cloud-client/ribbon-client/add?param1=13&param2=33&token=zz
+ * http://localhost:9477/cloud-client/ribbon-client/add?param1=13&param2=33&token=zz
  */
 public class CloudZuulClientTest {
     private static final String httpUrl = "http://localhost:9477/cloud-client/ribbon-client/add?param1=13&param2=33&token=zz";
@@ -18,20 +18,18 @@ public class CloudZuulClientTest {
     public static void main(String[] args) throws IOException {
 
 
-
-
     }
 
     @Test
     public void testCloudZuulClient1() throws IOException {
         String result = OkHttpClientUtil.httpPost(httpUrl, null);
-        System.out.println("result="+result);
+        System.out.println("result=" + result);
     }
 
     @Test
     public void testCloudZuulClient2() throws IOException {
         String result = OkHttpClientUtil.httpGet(httpUrl);
-        System.out.println("result="+result);
+        System.out.println("result=" + result);
     }
 
     @Test
@@ -42,9 +40,8 @@ public class CloudZuulClientTest {
         params.put("token", "zz");
 
         String result = OkHttpClientUtil.httpPost(httpCreateUrl, params);
-        System.out.println("result="+result);
+        System.out.println("result=" + result);
     }
-
 
 
 }
